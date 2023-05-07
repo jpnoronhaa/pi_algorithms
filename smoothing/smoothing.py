4f8cfa709ab4160808c384ee167aef9e0d4fa038
Joao Pedro Noronha <joaopdrons@gmail.com>
2023-05-07 15:01:38 -0300

    smoothing update

--- smoothing/smoothing.py
+++ smoothing/smoothing.py
@@ -6,16 +6,14 @@ grayscale_image.save('grayscale-cat.jpeg')
 smoothed_image = grayscale_image
 
 width, height = grayscale_image.size
-filter = [1,1,1,1,1,1,1,1,1]
+filter = [[1,1,1],[1,1,1],[1,1,1]]
 
-for i in range(1, width - 1):
-    for j in range(1, height - 1):
+for i in range(1, width - 2):
+    for j in range(1, height - 2):
         new_value = 0
-        filter_counter = 0
-        for k in range(i - 1, i + 2):
-            for l in range(j - 1, j + 2):
-                new_value += grayscale_image.getpixel((k,l)) * filter[filter_counter]
-                filter_counter += 1
+        for k in range(3):
+            for l in range(3):
+                new_value += grayscale_image.getpixel((k+i,l+j)) * filter[k][l]
         new_value = int(new_value/9)
         smoothed_image.putpixel((i,j), new_value)
 
